f82ebf5606a7a9e764e38310b773d17eade09d57
habubey <habubey@outlook.com>
2022-04-13 23:06:45 +0300

    all

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Header from './components/header/Header';
+import Card from './components/card/Card';
+
+function App() {
+  return (
+    <div className="main-container">   
+    
+     <Header/>
+   <Card/>
+    
+    </div>
+
+  );
+}
+
+export default App;
